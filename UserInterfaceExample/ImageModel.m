ee622bd7986d3a49927adf9410a5ae68c9792158
MuhAsh99 <mjashraf@smu.edu>
2022-09-01 20:27:47 -0500

    Initial Commit

---
+++ UserInterfaceExample/ImageModel.m
@@ -0,0 +1,62 @@
+//
+//  ImageModel.m
+//  UserInterfaceExample
+//
+//  Created by Eric Larson on 9/2/20.
+//  Copyright © 2020 Eric Larson. All rights reserved.
+//
+
+#import "ImageModel.h"
+
+@interface ImageModel ()
+
+@property (strong, nonatomic) NSArray* imageNames;
+@property (strong, nonatomic) NSDictionary* images;
+
+@end
+
+@implementation ImageModel
+@synthesize imageNames = _imageNames;
+@synthesize images = _images;
+
++(ImageModel*)sharedInstance{
+    static ImageModel* _sharedInstance = nil;
+    static dispatch_once_t predicate;
+    
+    dispatch_once(&predicate, ^{
+        _sharedInstance = [[ImageModel alloc] init];
+    } );
+    return _sharedInstance;
+}
+
+-(NSArray*) imageNames{
+    if(!_imageNames)
+        _imageNames = @[@"Bill",@"Eric",@"Jeff", @"hasb", @"harambe", @"carl"];
+    
+    return _imageNames; 
+}
+
+-(NSDictionary*) images{
+    if (!_images)
+        _images = @{@"Bill" : [UIImage imageNamed:@"Bill"], @"Eric" : [UIImage imageNamed:@"Eric"], @"Jeff" : [UIImage imageNamed:@"Jeff"], @"hasb" : [UIImage imageNamed:@"hasb"], @"harambe" : [UIImage imageNamed:@"harambe"], @"carl" : [UIImage imageNamed:@"carl"] };
+    return _images;
+}
+
+
+-(UIImage*)getImageWithName:(NSString*)name{
+    return [self.images objectForKey:name];
+}
+
+-(UIImage*)getImageWithIndex:(NSInteger)index{
+    return [self.images objectForKey:self.imageNames[index]];
+}
+
+-(NSInteger) numberOfImages{
+    return [self.images count];
+}
+
+-(NSString*) getImageNameForIndex:(NSInteger)index {
+    return self.imageNames[index];
+}
+
+@end
